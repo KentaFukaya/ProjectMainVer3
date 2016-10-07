68ed8485831b082f2f87b375655466177f197d1e
pickme0 <timallen1225@gmail.com>
2016-10-07 15:32:23 +0900

    zukan tuika

---
+++ app/src/main/java/com/example/b1014100_2/projectmainver3/zukan/ZukanActivity.java
@@ -0,0 +1,15 @@
+package com.example.b1014100_2.projectmainver3.zukan;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.b1014100_2.projectmainver3.R;
+
+public class ZukanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zukan);
+    }
+}
