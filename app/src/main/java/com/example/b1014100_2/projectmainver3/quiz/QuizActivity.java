a10980ad82a129f72526fdd96d7a951ecd04f6c3
b1014169 <b1014169@fun.ac.jp>
2016-11-01 23:00:29 +0900

    add Quiz.java add quizdata.db delete ainame_1.jpg

--- app/src/main/java/com/example/b1014100_2/projectmainver3/quiz/QuizActivity.java
+++ app/src/main/java/com/example/b1014100_2/projectmainver3/quiz/QuizActivity.java
@@ -1,6 +1,8 @@
 package com.example.b1014100_2.projectmainver3.quiz;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -13,21 +15,27 @@ import android.widget.TextView;
 import com.example.b1014100_2.projectmainver3.R;
 import com.example.b1014100_2.projectmainver3.zukan.ZukanActivity;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 
 /**
  * Created by b1014169 on 2016/10/07.
  */
 public class QuizActivity extends AppCompatActivity{
 
-    Button choices[] = new Button[3];
-    ImageView O_img;
-    ImageView X_img;
-    AlphaAnimation O_Anime;
-    AlphaAnimation X_Anime;
-    boolean correct;
+    private Button choices[] = new Button[3];
+    private ImageView O_img;
+    private ImageView X_img;
+    private AlphaAnimation O_Anime;
+    private AlphaAnimation X_Anime;
+    private boolean correct;
+    // database関係
+    private Quiz quiz;
+    private String DB_NAME = "quizdata.db";
+    private String TABLE_NAME = "quizdata";
+    private int DB_VERSION = 4;
+    private String[] FROM = {"_id", "name", "question", "choice1", "choice2", "choice3", "answer", "comment", "image_name"};
+    private String ORDER_BY = "_id" + " ASC";//並べる順
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +43,9 @@ public class QuizActivity extends AppCompatActivity{
         setContentView(R.layout.activity_quiz);
 
         // 図鑑から魚の id か何かを取得し、対応するクイズを表示
-        setQuiz(1);
-//        QuizSQLiteOpenFromAssets helper = new QuizSQLiteOpenFromAssets()
+        Intent intent = getIntent();
+        int id = intent.getIntExtra("ID", 0);
+        setQuiz(48);
 
         //アニメーションセット
         setAnime();
@@ -49,20 +58,38 @@ public class QuizActivity extends AppCompatActivity{
         X_img.setVisibility(View.GONE);
     }
 
-    // 1～63
+    // i問目のクイズを取得
     private void setQuiz(int i){
-        // クイズデータの取得
-        QuizData.set(i);
+        QuizSQLiteOpenFromAssets helper = new QuizSQLiteOpenFromAssets(this, DB_NAME, null, DB_VERSION);
+        SQLiteDatabase db = helper.getReadableDatabase();
+
+        Cursor c = db.query(TABLE_NAME, FROM, null, null, null, null, ORDER_BY);//queryの実行
+        c.moveToPosition(i - 1);
+        quiz= new Quiz();
+
+        quiz.setId(c.getInt(0));
+        quiz.setName(c.getString(1));
+        quiz.setQuestion(c.getString(2));
+        quiz.setChoice1(c.getString(3));
+        quiz.setChoice2(c.getString(4));
+        quiz.setChoice3(c.getString(5));
+        quiz.setAnswer(c.getString(6));
+        quiz.setComment(c.getString(7));
+        quiz.setImageName(c.getString(8));
 
         // 魚の画像を設定
         ImageView image = (ImageView) findViewById(R.id.quiz_image);
-        image.setImageResource(QuizData.imageId);
+        //文字列から画像のdrawableのIDを取得する
+        int imageId = getResources().getIdentifier(quiz.getImageName(), "drawable", this.getPackageName());
+        image.setImageResource(imageId);
         // 問題文の設定
         TextView question = (TextView) findViewById(R.id.quiz_question);
-        question.setText(QuizData.question);
+        question.setText(quiz.getQuestion());
         // 選択肢ボタンの設定
-        // 配列からListへ変換します。
-        List<String> list= Arrays.asList(QuizData.choices);
+        ArrayList<String> list= new ArrayList();
+        list.add(quiz.getChoice1());
+        list.add(quiz.getChoice2());
+        list.add(quiz.getChoice3());
         // リストの並びをシャッフルします。
         Collections.shuffle(list);
         choices[0] = (Button) findViewById(R.id.quiz_choice1);
@@ -75,7 +102,7 @@ public class QuizActivity extends AppCompatActivity{
         choices[0].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(choices[0].getText().toString().equals(QuizData.answer)) {
+                if(choices[0].getText().toString().equals(quiz.getAnswer()) || quiz.getId() == 7) {
                     // 正解演出
                     correct = true;
                     O_img.setVisibility(View.VISIBLE);
@@ -92,7 +119,7 @@ public class QuizActivity extends AppCompatActivity{
         choices[1].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(choices[1].getText().toString().equals(QuizData.answer)) {
+                if(choices[1].getText().toString().equals(quiz.getAnswer()) || quiz.getId() == 7) {
                     // 正解演出
                     correct = true;
                     O_img.setVisibility(View.VISIBLE);
@@ -109,7 +136,7 @@ public class QuizActivity extends AppCompatActivity{
         choices[2].setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(choices[2].getText().toString().equals(QuizData.answer)) {
+                if(choices[2].getText().toString().equals(quiz.getAnswer()) || quiz.getId() == 7) {
                     // 正解演出
                     correct = true;
                     O_img.setVisibility(View.VISIBLE);
@@ -124,7 +151,7 @@ public class QuizActivity extends AppCompatActivity{
         });
 
         // 戻るボタンの設定
-        Button r = (Button) findViewById(R.id.main_return_button);
+        Button r = (Button) findViewById(R.id.quiz_return_button);
         r.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -140,9 +167,9 @@ public class QuizActivity extends AppCompatActivity{
         setContentView(R.layout.activity_quiz2);
         // 正解・不正解の設定
         TextView O_X = (TextView) findViewById(R.id.quiz_correct);
-        ImageView ika = (ImageView) findViewById(R.id.result_ika);
+        ImageView ika = (ImageView) findViewById(R.id.quiz_ika2);
         ImageView maru_batsu = (ImageView) findViewById(R.id.quiz_maru_batsu);
-        if(correct) {
+        if(correct || quiz.getId() == 7) {
             O_X.setText("正解");
             ika.setImageResource(R.drawable.quiz_ika_happy);
             maru_batsu.setImageResource(R.drawable.quiz_hanamaru);
@@ -153,12 +180,12 @@ public class QuizActivity extends AppCompatActivity{
         }
         // 正解の選択肢の設定
         TextView answer = (TextView) findViewById(R.id.quiz_answer);
-        answer.setText(QuizData.getAnswer());
+        answer.setText("正解は・・・「" + quiz.getAnswer() + "」だよ");
         // 解説の設定
         TextView comment = (TextView) findViewById(R.id.quiz_comment);
-        comment.setText(QuizData.comment);
+        comment.setText(quiz.getComment());
         // 戻るボタンの設定
-        Button r = (Button) findViewById(R.id.result_return_button);
+        Button r = (Button) findViewById(R.id.quiz_return_button2);
         r.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
